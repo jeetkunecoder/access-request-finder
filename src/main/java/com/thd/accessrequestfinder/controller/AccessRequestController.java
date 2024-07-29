586c83e362b35dc89eb5928043980365bc597008
Carrasquel, Dario <DARIO_CARRASQUEL@homedepot.com>
2024-07-29 12:02:59 -0500

    Add test to trigger changes in JaCoCo report

--- src/main/java/com/thd/accessrequestfinder/controller/AccessRequestController.java
+++ src/main/java/com/thd/accessrequestfinder/controller/AccessRequestController.java
@@ -3,11 +3,14 @@ package com.thd.accessrequestfinder.controller;
 import com.thd.accessrequestfinder.model.AccessRequest;
 import com.thd.accessrequestfinder.service.AccessRequestService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("v1/access-request")
+@RestController
+@RequestMapping("/api/v1/access-request")
 public class AccessRequestController {
 
     private final AccessRequestService service;
@@ -16,8 +19,8 @@ public class AccessRequestController {
         this.service = service;
     }
 
-    @GetMapping("/find")
-    public List<AccessRequest> findAccessRequests(String team) {
-        return service.findAll();
+    @GetMapping(path = "/find/{team-id}")
+    public List<AccessRequest> findAccessRequests(@PathVariable("team-id") String teamId) {
+        return service.findAll(teamId);
     }
 }
--- src/test/java/com/thd/accessrequestfinder/AccessRequestFinderApplicationTests.java
+++ src/test/java/com/thd/accessrequestfinder/AccessRequestFinderApplicationTests.java
@@ -1,18 +1,42 @@
 package com.thd.accessrequestfinder;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+import com.thd.accessrequestfinder.controller.AccessRequestController;
+import com.thd.accessrequestfinder.model.AccessRequest;
+import com.thd.accessrequestfinder.service.AccessRequestService;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Collections;
+
+@WebMvcTest(AccessRequestController.class)
 class AccessRequestFinderApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private MockMvc mockMvc;
+	@MockBean
+	private AccessRequestService service;
 
 	@Test
-	void printHelloWorld() {
-		System.out.println("Hello World");
+	void getAccessRequestsShouldReturnOkStatus() throws Exception {
+		AccessRequest accessRequest = AccessRequest
+			.builder()
+			.id("id")
+			.name("name")
+			.description("description")
+			.teamId("team-id")
+			.build();
+
+		when(service.findAll("clearing"))
+			.thenReturn(Collections.singletonList(accessRequest));
+		this.mockMvc.perform(get("/api/v1/access-request/find/clearing")).andExpect(status().isOk());
 	}
 
 }
